bae65c9963bc902af6403fcc7a50f45e2ef48f27
Aidan Lalonde-Novales <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2022-03-30 14:28:40 -0400

    coded

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by Aidan Lalonde-Novales
+# Created March 2022
+# This file contains Learning Guide 01's code.
+
+import stage
+import ugame
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("\n\n\n")
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # placeholder
+
+if __name__ == "__main__":
+    game_scene()
