a155ca59feaa5f6095d03129bcbd994afeafcb97
Aidan Lalonde-Novales <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2022-04-05 14:37:41 -0400

    zarn

--- code.py
+++ code.py
@@ -10,15 +10,32 @@ import ugame
 
 def game_scene():
     # this function is the main game game_scene
+
+    # image banks
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # sets the background, 10x8
     background = stage.Grid(image_bank_background, 10, 8)
+
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
+    # creates a stage, sets to 60fps
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    # order of layers
+    game.layers = [ship] + [background]
+    # render the background and sprite list, most likely once per scene
     game.render_block()
 
     # repeat forever, game loop
     while True:
-        pass  # placeholder
+        # get user input
+
+        # update game logic
+
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
