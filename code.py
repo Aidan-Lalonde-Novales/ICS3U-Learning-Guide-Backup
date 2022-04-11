820472fbf72ecb40933cddef25edd567fa233c1f
Aidan Lalonde-Novales <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2022-04-11 14:18:21 -0400

    updated the code so the left and right walls are INPENETRABLE!!!!!

--- code.py
+++ code.py
@@ -7,6 +7,8 @@
 import stage
 import ugame
 
+import constants
+
 
 def game_scene():
     # this function is the main game game_scene
@@ -18,7 +20,7 @@ def game_scene():
     # sets the background, 10x8
     background = stage.Grid(image_bank_background, 10, 8)
 
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
 
     # creates a stage, sets to 60fps
     game = stage.Stage(ugame.display, 60)
@@ -32,21 +34,31 @@ def game_scene():
         # get user input
         keys = ugame.buttons.get_pressed()
 
-        if keys & ugame.K_X:
+        # A button to fire
+        if keys & ugame.K_X != 0:
             print("A")
-        if keys & ugame.K_O:
+        if keys & ugame.K_O != 0:
             print("B")
-        if keys & ugame.K_START:
+        if keys & ugame.K_START != 0:
             print("Start")
-        if keys & ugame.K_SELECT:
+        if keys & ugame.K_SELECT != 0:
             print("Select")
-        if keys & ugame.K_RIGHT:
-            ship.move(ship.x + 1, ship.y)
-        if keys & ugame.K_LEFT:
-            ship.move(ship.x - 1, ship.y)
-        if keys & ugame.K_UP:
+
+        if keys & ugame.K_RIGHT != 0:
+            if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
+                ship.move((ship.x + constants.SPRITE_MOVEMENT_SPEED), ship.y)
+            else:
+                ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
+                
+        if keys & ugame.K_LEFT != 0:
+            if ship.x > 0:
+                ship.move((ship.x - constants.SPRITE_MOVEMENT_SPEED), ship.y)
+            else:
+                ship.move(0, ship.y)
+
+        if keys & ugame.K_UP != 0:
             ship.move(ship.x, ship.y - 1)
-        if keys & ugame.K_DOWN:
+        if keys & ugame.K_DOWN != 0:
             ship.move(ship.x, ship.y + 1)
 
         # update game logic
